1bb4d79b2b1c917e5602afe1531baabaee6d5daa
Victor Lee <myleevictor@gmail.com>
2018-06-24 16:03:01 +0800

    refactored program with new methods to count words in text input and rank them by top ten

--- main.go
+++ main.go
@@ -5,23 +5,53 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"strings"
+	s "strings"
+	"sort"
+	"regexp"
 )
 
-func reverseWords(input string) string {
-	n := 0
-	rune := make([]rune, len(input))
-	for _, r := range input {
-		rune[n] = r
-		n++
-	}
-	rune = rune[0:n]
-	for i := 0; i < n/2; i++ {
-		rune[i], rune[n-1-i] = rune[n-1-i], rune[i]
-	}
-	output := string(rune)
-	return output
+func getWordsFrom(text string) []string {
+	words := regexp.MustCompile("\\w+")
+	return words.FindAllString(s.ToLower(text), -1)
 }
+
+func countWords(words []string) map[string]int {
+	wordCounts := make(map[string]int)
+	for _, word := range words {
+		wordCounts[word]++
+	}
+	return wordCounts
+}
+
+func consoleOut(orderedWordCounts PairList) {
+	for _, wordCount := range orderedWordCounts {
+		fmt.Printf("%v\n", wordCount)
+	}
+}
+
+func rankByWordCount(wordFrequencies map[string]int) PairList {
+	pl := make(PairList, len(wordFrequencies))
+	i := 0
+	for k, v := range wordFrequencies {
+		pl[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(sort.Reverse(pl))
+	slicedPl := pl[0:10]
+	return slicedPl
+}
+
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
 
 func findWordsInText(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
@@ -32,23 +62,24 @@ func findWordsInText(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Form["url_long"])
 	for key, value := range r.Form {
 		fmt.Println("key", key)
-		fmt.Println("val:", strings.Join(value, ""))
+		fmt.Println("val:", s.Join(value, ""))
 	}
 	fmt.Fprintf(w, "Hello & Welcome To My First GoLang Server")
 }
 
 func text(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method)
-	var result string
+	// var result PairList
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("form.gtpl")
 		t.Execute(w, nil)
 	} else {
 		r.ParseForm()
-		result = reverseWords(r.FormValue("text"))
-		fmt.Println("Top Ten Words:", reverseWords(r.FormValue("text")))
+		// result = reverseWords(r.FormValue("text"))
+		// result = rankByWordCount(countWords(getWordsFrom(r.FormValue("text"))))
+		fmt.Println("Top Ten Words:", rankByWordCount(countWords(getWordsFrom(r.FormValue("text")))))
 	}
-	w.Write([]byte(result))
+	
 }
 
 func main() {
